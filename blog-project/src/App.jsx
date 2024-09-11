03073b2a72f98da09fba463c442a21eb6b078363
thevaishndra <chandrav306m@gmail.com>
2024-09-12 00:10:43 +0530

    appwrite

---
+++ blog-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+  return (
+    <>
+      <div>blog with appwrite</div>
+    </>
+  )
+}
+
+export default App
