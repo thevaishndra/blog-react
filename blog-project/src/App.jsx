8a9a6afd3f951103383944ab4f90ec2ca34aaa8d
thevaishndra <chandrav306m@gmail.com>
2024-09-13 00:29:41 +0530

    database appwrite

--- blog-project/src/App.jsx
+++ blog-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <>
       <div>blog with appwrite</div>
