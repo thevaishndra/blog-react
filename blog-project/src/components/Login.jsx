bdb5bca05c8389e8a2f29456037be4d9c26f4805
thevaishndra <chandrav306m@gmail.com>
2024-09-18 17:46:32 +0530

    authentication

--- blog-project/src/components/Login.jsx
+++ blog-project/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import {Login as authLogin} from '../store/authSlice'
-import {Button, input, logo} from './index'
+import {Button, Input, Logo} from './index'
 import { useDispatch } from 'react-redux'
 import authService from '../appwrite/auth'
 import {useForm} from 'react-hook-form'
@@ -33,6 +33,35 @@ function Login() {
                 <Logo width="100%"/>
                 </span>
             </div>
+            <h2 className='text-center text-2xl font-bold leading-tight' >Sign in to your account</h2>
+            <p className='mt-2 text-center text-base text-black/60' >
+                 Don&apos;t have any account?&nbsp;
+                <link to="/signup" className='font-medium text-primary transition-all duration-200 hover:underline'>
+                Sign Up
+                </link>
+            </p>
+            {error && <p className='text-red-600 mt-8 text-center' >{error}</p>}
+            <form onSubmit={handleSubmit(login)} className='mt-8' >
+                <div className='space-y-5'>
+                    <Input label="Email:" placeholder="Enter your email" type="email" 
+                    {...register("email", {
+                        reequired: true,
+                        validate: {//resource -> regexr for this weird pattern or chatgpt
+                            matchPattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                            "Email address must be a valid address",
+                        }
+                    })} />
+
+                    <Input label="Email:" placeholder="Enter your email" type="email"
+                      {...register("password", {
+                        required: true,
+                      })}
+                    />
+                    <Button
+                    type="submit"
+                    className="w-full">Sign in</Button>
+                </div>
+            </form>
         </div>
     </div>
   )
--- blog-project/src/components/index.js
+++ blog-project/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 
-export {Header, Footer, Container, Logo, LogoutBtn} 
+export {Header, Footer, Container, Logo, LogoutBtn, Input} 
